244fef468802f31301bc15653082effa89c9b6ca
Gabriel-Campos <gabriel.campos@xenial.com>
2025-04-20 12:48:04 -0300

    estruturando sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,22 @@ import { LoginComponent } from './auth/login/login.component';
 import { GoogleCallbackComponent } from './auth/callback/google-callback.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './auth/guards/auth.guard';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { PageComponent } from './core/page/page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'auth/callback', component: GoogleCallbackComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  {
+    path: '',
+    component: PageComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'schedule', component: ScheduleComponent },
+    ]
+  },
   { path: '**', redirectTo: 'login' }
 ];
 
