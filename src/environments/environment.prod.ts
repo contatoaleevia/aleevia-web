ec3ab49e95bd43ac04d18c98cb5e3ab3776a0fea
Fabricio Yukio TM <fabricio.munhoz@leveenergia.com.br>
2025-05-13 18:04:38 -0300

    Adjusting backend location
    Adjusting backend location

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://homolog.aleevia.com/api/',
+  apiUrl: 'https://aleevia.cloud/service/',
   apiKey: '6a5e7add-7e24-48e0-90aa-79486597f1d3'
 };
